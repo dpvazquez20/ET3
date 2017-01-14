f1f29668a14e79c09bb9fe4e3d6b09bb0ce8d67f
Alberto Álvarez Vázquez <albertoalvarezvazquez@gmail.com>
2017-01-14 12:45:17 +0100

    subida
    Modelo producto

---
+++ modelos/PRODUCTO_Model.php
@@ -0,0 +1,89 @@
+<?php
+
+require('PRODUCTO.php');
+require_once ('../conectarBD.php');
+class Producto_modelo
+{
+    var $mysql2;
+    var $producto;
+
+    function __construct()
+    {
+        $this->mysql2 = conectarBD();
+        $this->material = new Producto();
+
+    }
+
+    function altaProducto($productoAInsertar){
+
+        $this->mysql = conectarBD();
+        $insertar = "INSERT INTO producto (nombre,descripcion) VALUES('".utf8_encode($productoAInsertar->getNombre())."','".utf8_encode($productoAInsertar->getDescripcion())."');";
+        if($this->mysql->query($insertar)){
+            return "Se añadio el producto correctamente";
+        }else{
+            return "No se ha podido añadir";
+        }
+
+    }
+
+
+    function deleteProducto($id_producto){
+
+        $this->mysql = conectarBD();
+        $sql = "DELETE FROM producto WHERE id_producto='".$id_producto."';";
+        if($this->mysql->query($sql)){
+            return"Se ha eliminado correctamente el producto";
+
+        }else{
+            return"Lo sentimos, no se ha podido eliminar el producto";
+        }
+    }
+
+    function modifyProducto($nombreAModificar, $descripcionAModificar, $productoModificar){
+        $this->mysql= conectarBD();
+        $sql = "UPDATE producto SET nombre='" . $nombreAModificar ."', descripcion='" . $descripcionAModificar ."' WHERE id_producto='".$productoModificar."';";
+        if($this->mysql->query($sql)){
+            return "El producto ha sido modificado correctamente";
+
+        }else{
+            return "El producto no ha podido modificarse";
+
+        }
+    }
+
+
+    public static function listarProducto(){
+    $conexion = conectarBD();
+    $sql = "SELECT * FROM producto";
+    $result = $conexion->query($sql);
+    return $result;
+
+    }
+    public static function getProducto($id){
+        $conexion= conectarBD();
+        $sql= "SELECT * from producto WHERE id_producto='".$id."';";
+        return $conexion->query($sql);
+    }
+
+    public static function getNombreProducto($id){
+        $conexion= conectarBD();
+        $sql= "SELECT * from producto WHERE nombre LIKE '".$id."%';";
+        return $conexion->query($sql);
+    }
+
+    public static function getDEsProducto($id){
+        $conexion= conectarBD();
+        $sql= "SELECT * from producto WHERE descripcion LIKE '".$id."%';";
+        return $conexion->query($sql);
+    }
+
+
+
+
+
+
+
+}
+
+
+?>

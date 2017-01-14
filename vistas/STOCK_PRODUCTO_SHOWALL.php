38b83a5bee8dc578177aabb6a87286ccc943ca7a
Alberto Álvarez Vázquez <albertoalvarezvazquez@gmail.com>
2017-01-14 17:25:00 +0100

    subida
    Modificado STOCK_PRODUCTO_SHOWALL.php para poder buscar por nombre, coste o fecha

--- vistas/STOCK_PRODUCTO_SHOWALL.php
+++ vistas/STOCK_PRODUCTO_SHOWALL.php
@@ -14,6 +14,7 @@ if(!isset($_SESSION)){
 require_once ('../modelos/CONTROLADOR_Model.php');
 require_once ('../modelos/STOCK_PRODUCTO_Model.php');
 require_once ('../modelos/MATERIAL_Model.php');
+require_once ('../modelos/PRODUCTO_Model.php');
 
 class Stock_producto_showAll{
 
@@ -29,6 +30,14 @@ class Stock_producto_showAll{
         <?php include ('menu.php');?>
         <!-- Título de la página -->
         <div class="col-sm-9">
+
+            <script>
+                $( function() {
+                    $( "#fecha" ).datepicker();
+                } );
+            </script>
+
+
             <title><?php echo $literales['listaStock']; ?></title>
             <div>
 
@@ -44,6 +53,66 @@ class Stock_producto_showAll{
                     }
                     ?>
                 </div>
+            </div>
+
+                <div class="col-sm-4">
+                    <form role="form" method="get">
+                        <input type="hidden" name="id" value="BUSCARPRODUCTO"/>
+                        <input type="hidden" name="ctr" value="PRODUCTO"/>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="input-group">
+                                    <div class="form-group">
+                                        <input placeholder="Nombre a buscar" type="text" id="nombreBuscar" name="nombreBuscar"
+                                               style="width: 200px" class=" form-control"> </input>
+                                    </div>
+                                    <span class="input-group-btn">
+                                    <button class="btn btn-default" type="submit">Buscar</button>
+                                </span>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+
+                    <form role="form" method="get">
+                        <input type="hidden" name="id" value="BUSCARPRODUCTO"/>
+                        <input type="hidden" name="ctr" value="PRODUCTO"/>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="input-group">
+                                    <div class="form-group">
+                                        <input placeholder="Coste a buscar" type="text" id="costeBuscar" name="costeBuscar"
+                                               style="width: 200px" class=" form-control"> </input>
+                                    </div>
+                                    <span class="input-group-btn">
+                                <button class="btn btn-default" type="submit">Buscar</button>
+                            </span>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            <div class="col-sm-4">
+                <form role="form" method="get">
+                    <input type="hidden" name="id" value="BUSCARPRODUCTO"/>
+                    <input type="hidden" name="ctr" value="PRODUCTO"/>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="input-group">
+                                <div class="form-group">
+                                    <input placeholder="Fecha a buscar" type="text" id="fecha" name="fecha"
+                                           style="width: 200px" class=" form-control"> </input>
+                                </div>
+                                <span class="input-group-btn">
+                                <button class="btn btn-default" type="submit">Buscar</button>
+                            </span>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
 
 
                 <table class=" table table-striped table-responsive">
@@ -58,37 +127,56 @@ class Stock_producto_showAll{
                     <tbody>
                     <?php
 
-                    $resul= Stock_producto_modelo::listarStock();
+                        if (isset($_GET['nombreBuscar'])) {
+                            $resul = Producto_modelo::getNombreProducto($_GET['nombreBuscar']);
+                        }
+                        else {
+                            if (isset($_GET['costeBuscar'])) {
 
-                    while($row = mysqli_fetch_assoc($resul)){
-                        $resultadoMaterial= Producto_modelo::getProducto($row['id_producto']);
-                        $rowMaterial = mysqli_fetch_assoc($resultadoMaterial);
+                                $resul = Stock_producto_modelo::getPorCoste($_GET['costeBuscar']);
 
-                        echo "<tr><td>" . $rowMaterial['nombre'] . "</td> <td>" . $row['coste'] . "</td><td>" . $row['fecha'] . "</td><td>";                        $listaControladores = Controlador_modelo::controladores(($_SESSION['perfil']));
-                        while($accion = mysqli_fetch_assoc($listaControladores)){
-                            if($accion['controlador']==$_GET['ctr']) {
-                                if ($accion['accion'] != "ADD") {
-                                    $estilo = 'btn btn-default';
-                                    $nombreBoton =$literales['ADD'];
-                                    if ($accion['accion'] == "EDIT") {
-                                        $estilo = 'btn btn-warning';
-                                        $nombreBoton =$literales['EDIT'];
-                                    }
+                            } else {
+                                if (isset($_GET['fecha'])){
 
-                                    if ($accion['accion'] == "SHOW") {
-                                        $estilo = 'btn btn-success';
-                                        $nombreBoton =$literales['SHOW'];
-                                    }
+                                    $resul = Stock_producto_modelo::getPorFecha($_GET['fecha']);
 
-                                    if ($accion['accion'] == "DELETE") {
-                                        $estilo = 'btn btn-danger';
-                                        $nombreBoton =$literales['DELETE'];
-                                    }
-                                    echo "<a href='../controladores/" . $accion['controlador'] . "_Controller.php?id=" . $accion['accion'] . $accion['controlador'] . "&idStock=" . $row['id'] . "'><button class='" . $estilo . "'>" .$nombreBoton . "</button></a>";                               }
+                                }else{
+
+                                    $resul= Stock_producto_modelo::listarStock();
+
+                                }
                             }
                         }
-                        echo"</td></tr>";
-                    }
+
+                        while($row = mysqli_fetch_assoc($resul)){
+                            $resultadoMaterial= Producto_modelo::getProducto($row['id_producto']);
+                            $rowMaterial = mysqli_fetch_assoc($resultadoMaterial);
+
+                            echo "<tr><td>" . $rowMaterial['nombre'] . "</td> <td>" . $row['coste'] . "</td><td>" . $row['fecha'] . "</td><td>";                        $listaControladores = Controlador_modelo::controladores(($_SESSION['perfil']));
+                            while($accion = mysqli_fetch_assoc($listaControladores)){
+                                if($accion['controlador']==$_GET['ctr']) {
+                                    if ($accion['accion'] != "ADD") {
+                                        $estilo = 'btn btn-default';
+                                        $nombreBoton =$literales['ADD'];
+                                        if ($accion['accion'] == "EDIT") {
+                                            $estilo = 'btn btn-warning';
+                                            $nombreBoton =$literales['EDIT'];
+                                        }
+
+                                        if ($accion['accion'] == "SHOW") {
+                                            $estilo = 'btn btn-success';
+                                            $nombreBoton =$literales['SHOW'];
+                                        }
+
+                                        if ($accion['accion'] == "DELETE") {
+                                            $estilo = 'btn btn-danger';
+                                            $nombreBoton =$literales['DELETE'];
+                                        }
+                                        echo "<a href='../controladores/" . $accion['controlador'] . "_Controller.php?id=" . $accion['accion'] . $accion['controlador'] . "&idStock=" . $row['id'] . "'><button class='" . $estilo . "'>" .$nombreBoton . "</button></a>";                               }
+                                }
+                            }
+                            echo"</td></tr>";
+                        }
                     ?>
 
                     </tbody>
